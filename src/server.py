8f17406abb2692f928f54e9e55c2b02f28efc796
Ryan Mower <ryancmower@gmail.com>
2021-06-29 21:52:47 -0500

    Server can now handle basic tasks like help, send, broadcast, and list.

--- src/server.py
+++ src/server.py
@@ -1,19 +1,25 @@
+#!/usr/bin/env python3
 import socket 
 import threading
 
+## ========  Config  ========
 HEADER = 64
 PORT = 5050
+#SERVER = '10.96.10.191'
 SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
 FORMAT = 'utf-8'
-DISCONNECT_MESSAGE = "!DISCONNECT"
+DISCONNECT_MESSAGE = "exit"
 
+
+## ========  Globals  ========
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
+connected_machines = []
 
+## ========  Functions  ========
 def handle_client(conn, addr):
-    print(f"[NEW CONNECTION] {addr} connected.")
-
+    conn.send(f"Connected to {SERVER}".encode(FORMAT))
     connected = True
     while connected:
         msg_length = conn.recv(HEADER).decode(FORMAT)
@@ -27,17 +33,74 @@ def handle_client(conn, addr):
             conn.send("Msg received".encode(FORMAT))
 
     conn.close()
-        
+       
+# Processes the user input
+def handle_user():
+
+    while True:
+        cmd = input("> ") 
+        cmd_lst = cmd.split()
+        if len(cmd_lst) <= 0:
+            continue
+
+        ## Broadcast
+        elif cmd_lst[0] == "broadcast":
+            new_cmd = ' '.join(cmd_lst[1:])
+            print(f'Sending \"{new_cmd}\" to...')
+            for data in connected_machines:
+                print(data['addr'][0])
+                new_cmd = ' '.join(cmd_lst[1:])
+                data['conn'].send(new_cmd.encode(FORMAT))
+
+        ## List
+        elif cmd_lst[0] == "list":
+            count = 1
+            for data in connected_machines:
+                print(f'{str(count)}) {data["addr"][0]}')
+                count = count + 1
+        ## Send
+        elif cmd_lst[0] == "send":
+            new_cmd = cmd_lst[1:]
+            if len(new_cmd) < 1:
+                print("Usage: send <ip> <cmd>")
+                continue
+            sent = False
+            for data in connected_machines:
+                if data['addr'][0] == new_cmd[0]:
+                    sent = True
+                    new_cmd = ' '.join(new_cmd[1:])
+                    print(f'Sending \"{new_cmd}\" to {data["addr"][0]}')
+                    data['conn'].send(new_cmd.encode(FORMAT))
+                    break
+            if not sent:
+                print(f'{new_cmd[0]} is not connected to the server.')
+
+        ## help
+        elif cmd_lst[0] == "help":
+            print("1. broadcast <cmd here>")
+            print("2. list")
+            print("3. send <ip> <cmd here>")
+            print("4. help")
+
+        ## Cmd not recognized
+        else:
+            print("Sorry, did not recognize that command.")
+    
 
 def start():
     server.listen()
-    print(f"[LISTENING] Server is listening on {SERVER}")
+    print(f"Server is listening on {SERVER}")
+    t = threading.Thread(target=handle_user, args=())
+    t.start()
     while True:
         conn, addr = server.accept()
+        data = {
+            'conn': conn,
+            'addr': addr,
+        }
+        connected_machines.append(data)
         thread = threading.Thread(target=handle_client, args=(conn, addr))
         thread.start()
-        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
 
 
-print("[STARTING] server is starting...")
 start()
